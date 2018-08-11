61405cba9ac6fc02e27aac07428ef74b547eea9a
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-08-11 21:35:02 +0530

    Update Dashserver.py

--- app/Dashserver.py
+++ app/Dashserver.py
@@ -12,5 +12,5 @@ DashServer = dash.Dash(
 
 DashServer.scripts.config.serve_locally=True
 DashServer.css.config.serve_locally=True
-DashServer.server.secret_key = os.environ.get('secret_key','secret')
+#DashServer.server.secret_key = os.environ.get('secret_key','secret')
 DashServer.config['suppress_callback_exceptions']=True

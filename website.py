495d3813a3b8ecc8a190b5b5b94824ac1d70ba3c
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-08-11 14:31:19 +0530

    Update website.py

--- website.py
+++ website.py
@@ -16,5 +16,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 myApp = DispatcherMiddleware(AppServer,{'/app': webServer})
 
 if __name__ == '__main__':
-   run_simple('127.0.0.1', 5000, myApp, use_reloader=True, use_debugger=True)
+   myApp.run_server(debug=True)
+   #run_simple('127.0.0.1', 5000, myApp, use_reloader=True, use_debugger=True)
+  
 

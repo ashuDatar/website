53f72b536b20b209c79312e3f96474cf00fc87e8
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-08-12 09:16:26 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -22,8 +22,24 @@ db = SQLAlchemy(AppServer)
 babel = Babel()
 babel.init_app(AppServer)
 db.init_app(AppServer)
-AppServer.elasticsearch = Elasticsearch([AppServer.config['ELASTICSEARCH_URL']]) \
-       if AppServer.config['ELASTICSEARCH_URL'] else None
+#AppServer.elasticsearch = Elasticsearch([AppServer.config['ELASTICSEARCH_URL']]) \
+#       if AppServer.config['ELASTICSEARCH_URL'] else None
+
+#BONSAI_URL = 'https://so749jc8in:874f1m1pfb@privet-467140.us-east-1.bonsaisearch.net'
+bonsai = 'https://so749jc8in:874f1m1pfb@privet-467140.us-east-1.bonsaisearch.net'
+auth = re.search('https\:\/\/(.*)\@', bonsai).group(1).split(':')
+host = bonsai.replace('https://%s:%s@' % (auth[0], auth[1]), '')
+
+es_header = [{
+  'host': host,
+  'port': 443,
+  'use_ssl': True,
+  'http_auth': (auth[0],auth[1])
+}]
+
+AppServer.elasticsearch = Elasticsearch(es_header)
+
+
 #migrate = Migrate(AppServer, db)
 
 from app import routes, models

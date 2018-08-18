c27641ce8315e3438aeb6e8cccde14df5e09e691
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-08-18 13:36:11 +0530

    Update dashapp1.py

--- app/MyDashApps/dashapp1.py
+++ app/MyDashApps/dashapp1.py
@@ -27,188 +27,107 @@ import dash_table_experiments as dt
 
 
 def select_chart(x_axis,y_axis,chart_type,file) :
-
     data_chart = file
-
     dataPanda = []
-
-    dataPanda = create_trace(data_chart,x_axis,y_axis,chart_type,dataPanda)
-
+    state_list = ['CHANDIGARH', 'HARYANA', 'HIMACHAL_PRADESH', 'JAMMU_KASHMIR','NCT_OF_DELHI','PUNJAB','RAJASTHAN',
+                  'ARUNACHAL_PRADESH','ASSAM','MANIPUR','MEGHALAYA','MIZORAM','NAGALAND','TRIPURA','ANDAMAN_NICOBAR_ISLANDS',
+                  'BIHAR','JHARKHAND','ODISHA','SIKKIM','WEST_BENGAL','CHHATTISGARH','MADHYA_PRADESH','UTTARAKHAND',
+                  'UTTAR_PRADESH','DADRANAGAR_HAVELI','DAMAN_DIU','GOA','GUJARAT','MAHARASHTRA','ANDHRA_PRADESH',
+                  'KARNATAKA','KERALA','LAKSHADWEEP','PUDUCHERRY','TAMIL_NADU','TELANGANA','All_India']
+    dataPanda = create_trace(data_chart,x_axis,y_axis,chart_type,dataPanda,state_list)
+    for i in range(0,36):
+        dataPanda[i]['visible'] = False
     return dataPanda
-
      
 
     
 
-def create_trace(data_chart,x_axis,y_axis,chart_type,dataPanda):    
-
+def create_trace(data_chart,x_axis,y_axis,chart_type,dataPanda,state_list):    
     if (x_axis == 'Date'):
-
-            dataPanda = create_date_trace(data_chart,x_axis,y_axis,chart_type,dataPanda)
-
+            dataPanda = create_date_trace(data_chart,x_axis,y_axis,chart_type,dataPanda,state_list)
     else:
-
-            dataPanda = create_other_trace(data_chart,x_axis,y_axis,chart_type,dataPanda)
-
+            dataPanda = create_other_trace(data_chart,x_axis,y_axis,chart_type,dataPanda,state_list)
     return dataPanda
 
 
 
-
-
-def create_date_trace(data_chart,x_axis,y_axis,chart_type,dataPanda) : 
-
+def create_date_trace(data_chart,x_axis,y_axis,chart_type,dataPanda,state_list) : 
     x=data_chart[data_chart['Description'] == y_axis]['Date']
-
     if (chart_type == 'scatter'): 
-
-            for i in data_chart.iloc[:,16:52].columns.unique():
-
+            for i in state_list:
                 trace = go.Scatter(
-
                     x=x,
-
                     y=data_chart[data_chart['Description'] == y_axis][i],
-
                     text= i,
-
                     mode='markers',
-
                     opacity=0.7,
-
                     marker={
-
                         'size': 15,
-
                         'line': {'width': 0.5, 'color': 'white'}
-
                     },
-
                     name=i  ) 
-
                 dataPanda.append(trace)
-
     else:            
-
         if (chart_type == 'line'): 
-
-            for i in data_chart.iloc[:,15:51].columns.unique():
-
+            for i in state_list:
                     trace = go.Scatter(
-
                             x=x,
-
                             y=data_chart[data_chart['Description'] == y_axis][i],
-
                             text= i,
-
                             mode = 'lines',
-
                             opacity=0.7,
-
                             name=i  ) 
-
                     dataPanda.append(trace)
-
         else:            
-
             if (chart_type == 'bar'): 
-
-                for i in data_chart.iloc[:,15:51].columns.unique():
-
+                for i in state_list:
                         trace = go.Bar(
-
                             x=x,
-
                             y=data_chart[data_chart['Description'] == y_axis][i],
-
                             text= i,
-
                             opacity=0.7,
-
                             name=i  ) 
-
                         dataPanda.append(trace)            
-
     return dataPanda
 
 
-
-def create_other_trace(data_chart,x_axis,y_axis,chart_type,dataPanda) : 
-
+def create_other_trace(data_chart,x_axis,y_axis,chart_type,dataPanda,state_list) : 
     if (chart_type == 'scatter'): 
-
-            for i in data_chart.iloc[:,16:52].columns.unique():
-
+            for i in state_list:
                 trace = go.Scatter(
-
-                    x=data_chart[data_chart['Description'] == x_axis][i],
-
-                    y=data_chart[data_chart['Description'] == y_axis][i],
-
+                    x=data_chart[data_chart['Metric'] == x_axis][i],
+                    y=data_chart[data_chart['Metric'] == y_axis][i],
                     text= i,
-
                     mode='markers',
-
                     opacity=0.7,
-
                     marker={
-
                         'size': 15,
-
                         'line': {'width': 0.5, 'color': 'white'}
-
                     },
-
                     name=i  ) 
-
                 dataPanda.append(trace)
-
     else :            
-
         if (chart_type == 'line'): 
-
-            for i in data_chart.iloc[:,16:52].columns.unique():
-
+            for i in state_list:
                     trace = go.Scatter(
-
-                                x=data_chart[data_chart['Description'] == x_axis][i],
-
-                                y=data_chart[data_chart['Description'] == y_axis][i],
-
+                                x=data_chart[data_chart['Metric'] == x_axis][i],
+                                y=data_chart[data_chart['Metric'] == y_axis][i],
                                 text= i,
-
                                 mode = 'lines',
-
                                 opacity=0.7,
-
                                 name=i  ) 
-
                     dataPanda.append(trace)
-
         else:            
-
             if (chart_type == 'bar'): 
-
-                    for i in data_chart.iloc[:,16:52].columns.unique():
-
+                    for i in state_list:
                             trace = go.Bar(
-
-                                    x=data_chart[data_chart['Description'] == x_axis][i],
-
-                                    y=data_chart[data_chart['Description'] == y_axis][i],
-
+                                    x=data_chart[data_chart['Metric'] == x_axis][i],
+                                    y=data_chart[data_chart['Metric'] == y_axis][i],
                                     text= i,
-
                                     opacity=0.7,
-
                                     name=i  ) 
-
                             dataPanda.append(trace)            
-
     return dataPanda
-
-
 
 
 
@@ -224,34 +143,88 @@ def create_layout(x_axis,y_axis) :
 
                 height=700,
 
-                xaxis = dict(title= x_axis),
+                xaxis = dict(title= 'Date'),
 
-                yaxis=dict(title=y_axis),
+                yaxis=dict(title='Outstanding loans of Scheduled commercial banks  in semi urban areas'),
 
-                legend=dict(orientation="h"),
-
-                hovermode='closest'
+                #legend=dict(orientation="h"),
+               
+                hovermode='closest',
+              
+                showlegend=True
 
             )
 
     return layout
 
 
+DashServer.layout = html.Div([
+    # title row
+    html.Div(
+        [
+            html.H1(
+                'States of India',
+                style={'font-family': 'Helvetica',
+                       "margin-top": "25",
+                       "margin-bottom": "0"},
+                className='eight columns',
+            ),
+             html.P(
+                'Explore states of India',
+                style={'font-family': 'Helvetica',
+                       "font-size": "120%",
+                       "width": "80%"},
+                className='eight columns',
+            ),
+        ], 
+
+         className='row'
+        ),
+    # Select visualization
 
 
+     html.Div( dt.DataTable(id='datatable', rows=[{}]) , style={'display': 'none'}),  
+    
+     html.Div(id='text'),
+    
+     #dcc.Location(id='url', refresh=False),
+    
 
-#
+    # selectors
 
-#Define x and y axis options
+     html.Div(
+        [
+       html.Div(
+        [
+            html.P('Choose chart-type:'), 
+            dcc.Dropdown(
+                    id='chart_type',
+                    options=[{'label': 'Scatter', 'value': 'scatter'},
+                             {'label': 'Line', 'value': 'line'},
+                             {'label': 'Bar', 'value': 'bar'}],
+                    value='bar'
+                        )
+            ],
+           className='four columns',
+           style={'margin-top': '10'}
+     )
 
+        ], className='row'
+   ),
 
+  #chart
+    html.Div(
+       [
+          dcc.Graph(id='example-graph',
+                              #animate=True, 
+                            style={'margin-top': '20'},
+                             config={'displayModeBar': False}
+                   )
+        ], className='row'
+         ),
 
-#x_axis_list = file['Metric'].unique().tolist()
-
-#y_axis_list = file['Metric'].unique().tolist()
-
-#x_axis_list.append('Date')
-
+    dcc.Markdown('Created by [Ashutosh Datar](https://twitter.com/adatar)')
+], className='ten columns offset-by-one')    
 
 
 
@@ -266,155 +239,44 @@ DashServer.css.append_css({'external_url':
 
 DashServer.title = 'States of India'
 
-#server = app.server
-
-
-
-DashServer.layout = html.Div([
-
-    # title row
-
-    html.Div(
-
-        [
-
-            html.H1(
-
-                'States of India - Explore states of India ',
-
-                style={'font-family': 'Helvetica',
-
-                       "margin-top": "25",
-
-                       "margin-bottom": "0"},
-
-                className='eight columns',
-
-            ),
-
-             html.P(
-
-                'Explore states of India',
-
-                style={'font-family': 'Helvetica',
-
-                       "font-size": "120%",
-
-                       "width": "80%"},
-
-                className='eight columns',
-
-            ),
-
-        ], 
-
-         className='row'
-
-        ),
-
-    
-    
-     html.Div( dt.DataTable(id='datatable', rows=[{}]) , style={'display': 'none'}),  
-    
-    # Select visualization
-
-   
-
-    # selectors
-
-     html.Div(
-
-        [
-
-        
-
-    
-
-       html.Div(
-
-        [
-
-            html.P('Choose chart-type:'), 
-
-            dcc.Dropdown(
-
-                    id='chart_type',
-
-                    options=[{'label': 'Scatter', 'value': 'scatter'},
-
-                             {'label': 'Line', 'value': 'line'},
-
-                             {'label': 'Bar', 'value': 'bar'}],
-
-                    value='scatter'
-
-                        )
-
-            ],
-
-           className='four columns',
-
-           style={'margin-top': '10'}
-
-     )
-
-        ], className='row'
-
-   ),
-
-    
-
-  #chart
-
-     html.Div(
-
-        [
-
-            dcc.Graph(id='example-graph',
-
-                              #animate=True, 
-
-                              style={'margin-top': '20'},
-
-                               config={'displayModeBar': False}
-
-                     )
-
-        ], className='row'
-
-         ),
-
-    
-
-    dcc.Markdown('Created by [Ashutosh Datar](https://twitter.com/adatar)')
-
-   
-
-], className='ten columns offset-by-one')    
-
-
-
-
 @DashServer.callback(
-    dash.dependencies.Output('datatable', 'rows'))
-def update_data():
+    dash.dependencies.Output('datatable', 'rows'),[dash.dependencies.Input('chart_type', 'value')])
+def update_data(chart_type):
     data = db.session.query(test_data_dummy_data)
     file = pd.read_sql(data.statement, data.session.bind)
-#    file = pd.read_csv('Test_Data_Dummy_Data.csv')
+    #file = pd.read_csv('C:\\Users\\Pranoti\\statesofindia\\website\\Test_Data_Dummy_Data.csv')
     file.iloc[:,16:52] = file.iloc[:,16:52].apply(lambda x : x.astype('float'))
     file.iloc[:,16:52] = file.iloc[:,16:52].apply(lambda x : round(x, 2))
     cleaned_df = file.to_dict('records')
-    return cleaned_df 
+    #dataframe = file.to_json(date_format='iso', orient='split')
+    return cleaned_df
 
+
+@DashServer.callback(
+    dash.dependencies.Output('text', 'children'),
+    [dash.dependencies.Input('chart_type', 'value'),
+     dash.dependencies.Input('datatable', 'rows'),
+    ])
+def update_output(chart_type, rows):
+    description = str('Outstanding loans of Scheduled commercial banks  in semi urban areas')
+    file = pd.DataFrame(rows)
+    category = file[file['Description'] == description].Category.unique()
+    source = file[file['Description'] == description].Source.unique()
+    return html.Div([
+        html.H3('Visualization for {}'.format(description)),
+        html.H4('Series:{}'.format(category)),
+        html.H6('Source:{}'.format(source))
+         ])
 
 @DashServer.callback(
     dash.dependencies.Output('example-graph', 'figure'),
     [dash.dependencies.Input('chart_type', 'value'),
-     dash.dependencies.Input('url', 'pathname'),
      dash.dependencies.Input('datatable', 'rows'),
+     dash.dependencies.Input('url', 'pathname')
     ])
-def update_output(chart_type, pathname, rows):
+def update_output(chart_type, rows, pathname):
     des = str(pathname)
+    #des = str('Outstanding loans of Scheduled commercial banks  in semi urban areas')
     filter = des.split('/')[-1]
     filter = urllib.parse.unquote(filter)
     #data = db.session.query(test_data_dummy_data)
@@ -422,6 +284,7 @@ def update_output(chart_type, pathname, rows):
     #file = pd.read_csv('Test_Data_Dummy_Data.csv')
     #file.iloc[:,15:51] = file.iloc[:,16:52].apply(lambda x : x.astype('float'))
     #file.iloc[:,15:51] = file.iloc[:,16:52].apply(lambda x : round(x, 2))
+    #file = pd.DataFrame.from_dict(datatable, orient='index')
     file = pd.DataFrame(rows)
     x_axis = 'Date'
     y_axis = filter
@@ -429,7 +292,7 @@ def update_output(chart_type, pathname, rows):
     layout = create_layout(x_axis,y_axis)
     figure = {'data': dataPanda,
               'layout': layout}
-    return figure  
+    return figure
 
 
 layout = DashServer.layout

880ee10a7a67de40ee31d722bbb69dc7d6f8e296
pranoti-d <33633867+pranoti-d@users.noreply.github.com>
2018-08-20 11:21:56 +0530

    Update dashapp1.py

--- app/MyDashApps/dashapp1.py
+++ app/MyDashApps/dashapp1.py
@@ -372,7 +372,7 @@ def update_output(chart_type, pathname, state):
  #       return {'display': 'block'}
 
 
-@DashServer.callback(Output('output-container-button1', 'style'), [Input('button', 'n_clicks')])
+@DashServer.callback(Output('output-container-button1', 'children'), [Input('button', 'n_clicks')])
 def toggle_container(n_clicks):
     return 'The button has been clicked {} times'.format(n_clicks)
     #if n_clicks == 0:

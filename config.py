6eb502a33063484992003178c669f91106ff0ae6
ashuDatar <43102973+ashuDatar@users.noreply.github.com>
2018-09-09 08:19:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    ELASTICSEARCH_URL= 'https://nol0b9gns3:pfh02ezorl@state-8851386751.us-west-2.bonsaisearch.net'
-    MYSQL_HOST = 'us-cdbr-iron-east-01.cleardb.net'
-    MYSQL_USER = 'b52f569efaaf0b'
-    MYSQL_PASSWORD = '8e4e9d41'
-    MYSQL_DB = 'heroku_9c26c6619d3062f'
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://b52f569efaaf0b:8e4e9d41@us-cdbr-iron-east-01.cleardb.net/heroku_9c26c6619d3062f'
+    ELASTICSEARCH_URL= ''
+    MYSQL_HOST = ''
+    MYSQL_USER = ''
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = ''
+    SECRET_KEY = os.environ.get('SECRET_KEY') or ''
+    SQLALCHEMY_DATABASE_URI = ''
     SQLALCHEMY_TRACK_MODIFICATIONS = False
   

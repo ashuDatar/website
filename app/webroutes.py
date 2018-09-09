5408a4d3e6edbb3f1d76128e71306c7af3d847f1
ashuDatar <43102973+ashuDatar@users.noreply.github.com>
2018-09-09 08:19:29 +0530

    Delete webroutes.py

--- app/webroutes.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from datetime import datetime
-from flask import render_template, redirect
-
-webServer = Flask(__name__)
-
-@webServer.route('/contact')
-def contact():
-    return render_template('visualization.html')

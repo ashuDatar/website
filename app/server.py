e4afb0228946d51b94c533da52d5cdc3d6858e44
ashuDatar <43102973+ashuDatar@users.noreply.github.com>
2018-09-30 13:28:35 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -21,14 +21,7 @@ AppServer.config.from_object(Config)
 db = SQLAlchemy(AppServer)
 babel = Babel()
 babel.init_app(AppServer)
-#db.init_app(AppServer)
-#AppServer.elasticsearch = Elasticsearch([AppServer.config['ELASTICSEARCH_URL']]) \
-#       if AppServer.config['ELASTICSEARCH_URL'] else None
 
-#BONSAI_URL = 'https://so749jc8in:874f1m1pfb@privet-467140.us-east-1.bonsaisearch.net'
-bonsai = 'https://so749jc8in:874f1m1pfb@privet-467140.us-east-1.bonsaisearch.net'
-auth = re.search('https\:\/\/(.*)\@', bonsai).group(1).split(':')
-host = bonsai.replace('https://%s:%s@' % (auth[0], auth[1]), '')
 
 es_header = [{
   'host': host,
@@ -44,7 +37,4 @@ AppServer.elasticsearch = Elasticsearch(es_header)
 
 from app import routes, models
 
-#@AppServer.route('/', methods=['GET', 'POST'])
-#@AppServer.route('/index', methods=['GET', 'POST'])
-#def index():
-    #return render_template('visualization.html')
+

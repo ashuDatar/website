ae1a3c6f1cbf0161ee49135e9248e3b21a5a5329
ashuDatar <43102973+ashuDatar@users.noreply.github.com>
2018-10-02 21:53:51 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,10 @@
 from app.server import AppServer
-from app.Dashserver import DashServer
 from flask import render_template, flash, redirect, url_for, request, g , current_app
 #from app import server
 from flask import g
 from flask_babel import _, get_locale
-#from app.models import search_index
-#from app.forms import SearchForm, resultForm
-#from app.search import add_to_index
-from app.MyDashApps import dashapp0
-from app.MyDashApps import dashapp1
-from dash.dependencies import Input, State, Output
-import dash_html_components as html
-import dash
-import dash_core_components as dcc
+
+
 
 
 
